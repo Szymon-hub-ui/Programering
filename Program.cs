6307794c682710a9a5442d3110e9295787341da7
Szymon-hub-ui <szgo07001@utb.vaxjo.se>
2024-09-23 10:09:34 +0200

    Gissningspel

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(1, 101);
+int userGuess = 0;
+int guessCount = 0;
+while (userGuess != randomNumber)
+{
+    Console.WriteLine("Guess a number between 1 and 100");
+    string input = Console.ReadLine();
+    bool isValidNumber = int.TryParse(input, out userGuess);
+    if (!isValidNumber)
+    {
+        Console.WriteLine("Please enter a valid number");
+        continue;
+    }
+    guessCount++;
+    if (userGuess < randomNumber)
+    {
+        Console.WriteLine("Too low, guess higher");
+    }
+    else if (userGuess > randomNumber)
+    {
+        Console.WriteLine("Too high, guess lowe");
+    }
+}
+Console.WriteLine($"Congratulations! You guessed the number {randomNumber} in {guessCount} guesses");
