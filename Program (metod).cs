3be91920c0387da15bef202ff700524fa8feba68
Szymon-hub-ui <szgo07001@utb.vaxjo.se>
2024-10-14 10:06:09 +0200

    Add files via upload

---
+++ Program (metod).cs
@@ -0,0 +1,11 @@
+﻿int a=10;
+int b=20;
+BytPlats (ref a, ref b );
+Console.WriteLine (a+" "+b);
+
+void BytPlats (ref int a, ref int b) {
+    //a:10   b:20
+    int c = a;
+    a = b;
+    b = c;
+}
